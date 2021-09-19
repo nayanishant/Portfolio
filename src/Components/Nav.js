61a61d7b1e38014e3a560ecfb997543345ae5fec
nyanishant <nayanishant.info@gmail.com>
2021-09-19 23:14:30 +0530

    Basic design of nav bar

---
+++ src/Components/Nav.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+class NavBar extends Component {
+    render() {
+        return (
+            <div className="top">
+                <img src="#" alt="#" />
+                <ul>
+                    <li>Home</li>
+                    <li>Projects</li>
+                    <li>About</li>
+                    <li>Contacts</li>
+                </ul>
+            </div>
+        );
+    }
+}
+
+export default NavBar;
