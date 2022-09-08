a4db8cd9b5d22022100055186f4b688cce5c5f01
nyanishant <nayanishant.info@gmail.com>
2022-09-08 15:58:10 +0530

    Used map() in project section

--- src/Components/Projects.jsx
+++ src/Components/Projects.jsx
@@ -1,38 +1,31 @@
-import React, { Component } from 'react'
-import sheepyShop from '../Gallery/SheepyShops - Google Chrome 10-07-2021 12_28_23.png'
-import hackClub from '../Gallery/Hack Club _ PESCE — Mozilla Firefox 23-09-2021 00_03_24.png'
-import personalSite from '../Gallery/React App - Google Chrome 23-09-2021 10_09_22.png'
+import React from 'react'
+// import sheepyShop from '../Gallery/SheepyShops - Google Chrome 10-07-2021 12_28_23.png'
+// import hackClub from '../Gallery/Hack Club _ PESCE — Mozilla Firefox 23-09-2021 00_03_24.png'
+// import personalSite from '../Gallery/React App - Google Chrome 23-09-2021 10_09_22.png'
+import projectData from "../Data/ProjectsData"
 
-class Projects extends Component {
-    render() {
+const Projects = () => {
         return (
             <div className="projectWrap" id="project0">
                 <div className="name1">
                     <h3>My Projects</h3>
                 </div>
                 <div className="cardWrap">
-                    <div className="card">
-                        <img src={sheepyShop} alt="#"/>
-                        <a href="https://github.com/nayanishant/SheepyShops" target="_blank" rel="noopener noreferrer">
-                            SheepyShops
-                        </a>
-                    </div>
-                    <div className="card">
-                        <img src={hackClub} alt="#"/>
-                        <a href="https://github.com/Gautam-flash/hc_pesce_site" target="_blank" rel="noopener noreferrer">
-                            Hack Club's Website
-                        </a>
-                    </div>
-                    <div className="card">
-                        <img src={personalSite} alt="#"/>
-                        <a href="https://github.com/nayanishant/Portfolio" target="_blank" rel="noopener noreferrer">
-                            Portfolio
-                        </a>
-                    </div>
+                    {
+                        projectData.map((data) => {
+                            return (
+                                <div className="card" id=     {data.id}>
+                                    <img src={data.image} alt="#"/>
+                                    <a href={data.url} target="_blank" rel="noopener noreferrer">
+                                        {data.name}
+                                    </a>
+                                </div>
+                            )
+                        })
+                    }   
                 </div>
             </div>
         );
     }
-}
 
 export default Projects;
---
+++ src/Data/ProjectsData.js
@@ -0,0 +1,28 @@
+const projectData = [
+    {
+        "id": 1,
+        "name": "SheepyShops",
+        "url": "https://github.com/nayanishant/SheepyShops",
+        "image": "Gallery/SheepyShops.png"
+    },
+    {
+        "id": 2,
+        "name": "HackClub",
+        "url": "https://github.com/Gautam-flash/hc_pesce_site",
+        "image": "Gallery/Hack Club.png"
+    },
+    {
+        "id": 3,
+        "name": "Portfolio",
+        "url": "https://github.com/nayanishant/Portfolio",
+        "image": "Gallery/Portfolio.png"
+    },
+    {
+        "id": 4,
+        "name": "IoT Mini Project",
+        "url": "https://github.com/nayanishant/iot",
+        "image": "Gallery/IoTMini.png"
+    }
+]
+
+export default projectData
