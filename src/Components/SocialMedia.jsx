58f6872af2e2e8d285bcae962e9c9bb3c55a72fc
nyanishant <nayanishant.info@gmail.com>
2022-09-14 17:03:01 +0530

    Added new component

---
+++ src/Components/SocialMedia.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const SocialMedia = () => {
+  return (
+    <div className="social-links">
+        <a href="http://www.linkedin.com/in/nishant-nayan-773480193/" target="_blank" rel="noopener noreferrer">
+            <i class="fab fa-linkedin-in"></i>
+        </a>
+        <a href="https://github.com/nayanishant" target="_blank" rel="noopener noreferrer">
+            <i class="fab fa-github"></i>
+        </a>
+        <a href="https://twitter.com/Nishant70607283" target="_blank" rel="noopener noreferrer">
+            <i class="fab fa-twitter"></i>
+        </a>
+        <a href="mailto:nayanishant.info@gamil.com" target="_blank" rel="noopener noreferrer">
+            <i class="fas fa-envelope-open"></i>
+        </a>
+    </div>
+  )
+}
+
+export default SocialMedia
